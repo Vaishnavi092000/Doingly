eab75dda99d6d402a66015826a9c0cfb2179babb
unknown <vaishnavigulwelkar922@gmail.com>
2023-03-21 18:24:47 +0530

    some issue in toggle todo

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -36,10 +36,11 @@ export class TodoListComponent {
     console.log(todo);
     this.todos.push(todo);
     localStorage.setItem("todos",JSON.stringify(this.todos));
-    console.log(this.localItem);
+    console.log(todo);
   }  
 
   deleteItem(i :number){
+    console.log('delete index : ',i);
     this.todos.splice(i, 1);
     localStorage.setItem("todos",JSON.stringify(this.todos));
     this.todos[i].active = !this.todos[i].active;
@@ -47,6 +48,7 @@ export class TodoListComponent {
   }
 
   deleteDoneItem(i :number){
+    console.log('delete done index : ',i);
     this.donetodos.splice(i, 1);
     localStorage.setItem("donetodos",JSON.stringify(this.donetodos));
     // this.donetodos[i].active = !this.donetodos[i].active;
@@ -56,14 +58,19 @@ export class TodoListComponent {
   toggleTodo(todo: Todo){
     console.log('This todo : ',todo);
     const index = this.todos.indexOf(todo);
+    console.log('index in toggle : ',index);
+
+    console.log('Status in toggle todo : ',this.todos[index].active);
     this.todos[index].active = false;
-    this.donetodos.push(this.todos[index]);
+    this.donetodos.push(this.todo);
+
     console.log('Done Task : ', this.donetodos);
     localStorage.setItem("donetodos",JSON.stringify(this.donetodos));
+
     this.todos.splice(index, 1);
     localStorage.setItem("todos",JSON.stringify(this.todos));
+    console.log('Todos after delete in toggle : ',this.todos);
   }
-
 
 }
 
